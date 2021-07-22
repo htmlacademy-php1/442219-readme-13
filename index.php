e78f6bd140e6523fe589b26a93900a73a7fde048
IgorViV <iupgrader@mail.ru>
2021-07-22 15:25:14 +0300

    Добавляет имя пользователя

--- index.php
+++ index.php
@@ -68,7 +68,7 @@ $user_name = 'Игорь Влащенко';
                             </div>
                             <div class="header__profile-name">
                                 <span>
-                                    <!--здесь должно быть имя пользователя-->
+                                    <?=$user_name;?>
                                 </span>
                                 <svg class="header__link-arrow" width="10" height="6">
                                     <use xlink:href="#icon-arrow-right-ad"></use>
