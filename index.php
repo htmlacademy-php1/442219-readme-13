fc8e725e7602ea40d80a5fa2d7ec2e7e6b4cd2a9
IgorViV <iupgrader@mail.ru>
2021-07-28 18:43:14 +0300

    Дорабатывает шаблонизацию проекта

--- index.php
+++ index.php
@@ -1,45 +1,7 @@
 <?php
-    $is_auth = rand(0, 1);
-
-    $user_name = 'Игорь Влащенко';
-
-    $posts = [
-        [
-            'title' => 'Цитата',
-            'type' => 'post-quote',
-            'content' => 'Мы в жизни любим только раз, а после ищем лишь похожих',
-            'name' => 'Лариса', 'avatar' => 'userpic-larisa-small.jpg',
-        ],
-        [
-            'title' => 'Игра престолов',
-            'type' => 'post-text',
-            'content' => 'Не могу дождаться, начала финального сезона своего любимого сериала!',
-            'name' => 'Владик',
-            'avatar' => 'userpic.jpg',
-        ],
-        [
-            'title' => 'Наконец, обработал фотки!',
-            'type' => 'post-photo',
-            'content' => 'rock-medium.jpg',
-            'name' => 'Виктор',
-            'avatar' => 'userpic-mark.jpg',
-        ],
-        [
-            'title' => 'Моя мечта',
-            'type' => 'post-photo',
-            'content' => 'coast-medium.jpg',
-            'name' => 'Лариса',
-            'avatar' => 'userpic-larisa-small.jpg',
-        ],
-        [
-            'title' => 'Лучшие курсы',
-            'type' => 'post-link',
-            'content' => 'www.htmlacademy.ru',
-            'name' => 'Владик',
-            'avatar' => 'userpic.jpg',
-        ],
-    ];
-
+    require_once('helpers.php');
+    require_once('data.php');
+    
     function cut_text($text, $length = 300) {
 
         if (mb_strlen($text, 'UTF-8') <= $length) {
@@ -62,34 +24,9 @@
         return array($output_string, true);
     }
 
-    /**
-     * Подключает шаблон, передает туда данные и возвращает итоговый HTML контент
-     * @param string $name Путь к файлу шаблона относительно папки templates
-     * @param array $data Ассоциативный массив с данными для шаблона
-     * @return string Итоговый HTML
-     */
-    function include_template($name, array $data = [])
-    {
-        $name = 'templates/' . $name;
-        $result = '';
-
-        if (!is_readable($name)) {
-            return $result;
-        }
-
-        ob_start();
-        extract($data);
-        require $name;
-
-        $result = ob_get_clean();
-
-        return $result;
-    }
-
     $page_content = include_template('main.php', ['posts' => $posts]);
 
     $layout_content = include_template('layout.php', ['is_auth' => $is_auth, 'user_name' => $user_name,'content' => $page_content, 'title' => 'readme: популярное']);
 
     print($layout_content);
-
 ?>
---
+++ templates/post.php
@@ -0,0 +1,92 @@
+<article class="popular__post post <?= $value['type']; ?>">
+                <header class="post__header">
+                    <h2><?= htmlspecialchars($value['title']); ?></h2>
+                </header>
+                <div class="post__main">
+                    <?php if($value['type'] == 'post-quote'): ?>
+                        <blockquote>
+                            <p>
+                                <?= htmlspecialchars($value['content']);?>
+                            </p>
+                            <cite>Неизвестный Автор</cite>
+                        </blockquote>
+                    <?php elseif ($value['type'] == 'post-text'): ?>
+                        <?php $arr_content = cut_text(htmlspecialchars($value['content'])) ?>
+                        <?php if ($arr_content[1]): ?>
+                            <p>
+                                <?=$arr_content[0] . '...'; ?>
+                            </p>
+                            <a class="post-text__more-link" href="#">Читать далее</a>
+                            <?php else: ?>
+                            <p>
+                                <?= htmlspecialchars($value['content']); ?>
+                            </p>
+                        <?php endif; ?>
+                    <?php elseif ($value['type'] == 'post-photo'): ?>
+                        <div class="post-photo__image-wrapper">
+                            <img src="img/<?= $value['content']; ?>" alt="Фото от пользователя" width="360" height="240">
+                        </div>
+                    <?php elseif ($value['type'] == 'post-video'): ?>
+                        <div class="post-video__block">
+                            <div class="post-video__preview">
+                                <?= embed_youtube_cover($value['content']); ?>
+                                <img src="img/coast-medium.jpg" alt="Превью к видео" width="360" height="188">
+                            </div>
+                            <a href="post-details.html" class="post-video__play-big button">
+                                <svg class="post-video__play-big-icon" width="14" height="14">
+                                    <use xlink:href="#icon-video-play-big"></use>
+                                </svg>
+                                <span class="visually-hidden">Запустить проигрыватель</span>
+                            </a>
+                        </div>
+                    <?php elseif ($value['type'] == 'post-link'): ?>
+                        <div class="post-link__wrapper">
+                            <a class="post-link__external" href="http://<?= $value['content']; ?>" title="Перейти по ссылке">
+                                <div class="post-link__info-wrapper">
+                                    <div class="post-link__icon-wrapper">
+                                        <img src="https://www.google.com/s2/favicons?domain=vitadental.ru" alt="Иконка">
+                                    </div>
+                                    <div class="post-link__info">
+                                        <h3><?= htmlspecialchars($value['title']); ?></h3>
+                                    </div>
+                                </div>
+                                <span><?= htmlspecialchars($value['content']); ?></span>
+                            </a>
+                        </div>
+                    <?php endif; ?>
+                </div>
+                <footer class="post__footer">
+                    <div class="post__author">
+                        <a class="post__author-link" href="#" title="Автор">
+                            <div class="post__avatar-wrapper">
+                                <img class="post__author-avatar" src="img/<?= $value['avatar']; ?>" alt="Аватар пользователя">
+                            </div>
+                            <div class="post__info">
+                                <b class="post__author-name"><?= htmlspecialchars($value['name']); ?></b>
+                                <time class="post__time" datetime="">дата</time>
+                            </div>
+                        </a>
+                    </div>
+                    <div class="post__indicators">
+                        <div class="post__buttons">
+                            <a class="post__indicator post__indicator--likes button" href="#" title="Лайк">
+                                <svg class="post__indicator-icon" width="20" height="17">
+                                    <use xlink:href="#icon-heart"></use>
+                                </svg>
+                                <svg class="post__indicator-icon post__indicator-icon--like-active" width="20" height="17">
+                                    <use xlink:href="#icon-heart-active"></use>
+                                </svg>
+                                <span>0</span>
+                                <span class="visually-hidden">количество лайков</span>
+                            </a>
+                            <a class="post__indicator post__indicator--comments button" href="#" title="Комментарии">
+                                <svg class="post__indicator-icon" width="19" height="17">
+                                    <use xlink:href="#icon-comment"></use>
+                                </svg>
+                                <span>0</span>
+                                <span class="visually-hidden">количество комментариев</span>
+                            </a>
+                        </div>
+                    </div>
+                </footer>
+            </article>
