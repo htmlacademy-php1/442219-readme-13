7596f2c1e6e71c4fb642da3f97c603ee70104eec
IgorViV <iupgrader@mail.ru>
2021-08-18 13:13:14 +0300

    Оптимизирует проверку выполнения SQL запроса

--- index.php
+++ index.php
@@ -14,25 +14,28 @@
         mysqli_set_charset($link, "utf8");
         // SQL-запрос для получения типов контента
         $sql_types = 'SELECT title, class FROM types;';
+
         // SQL-запрос для получения списка постов, объединённых с пользователями и отсортированный по популярности
         $sql_posts = 'SELECT COUNT(likes.id) likes, posts.title, posts.text_content, posts.img_url, posts.video_url, posts.site_url, user_name AS author, types.class, users.avatar_url FROM posts JOIN users ON posts.user_id = users.id JOIN types ON posts.type_id = types.id JOIN likes ON posts.id = likes.post_id GROUP BY posts.id ORDER BY COUNT(likes.id) DESC;';
 
         $types = get_arr_from_mysql($link, $sql_types);
-        if (!$types) {
-            $page_content = get_error_template($link);
-        }
 
-        $posts = get_arr_from_mysql($link, $sql_posts);
-        if (!$posts) {
-            $page_content = get_error_template($link);
-        }
+        if ($types) {
+            $posts = get_arr_from_mysql($link, $sql_posts);
 
-        if ($types && $posts) {
-            $page_content = include_template('main.php', [
-                'posts' => $posts,
-                'types' => $types,
-            ]);
-        }
+            if ($posts) {
+                $page_content = include_template('main.php', [
+                    'posts' => $posts,
+                    'types' => $types,
+                ]);
+            } else {
+                $page_content = get_error_template($link);
+            };
+
+        } else {
+            $page_content = get_error_template($link);
+        };
+
     };
 
     /**
